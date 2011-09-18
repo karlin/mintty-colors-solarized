2a7dc80d29873f7b6ad39f5a79f47a704764e65b
karlin <karlin@atomicobject.com>
2011-09-18 16:53:06 -0400

    added light variant, with duplicated colors for now

---
+++ mintty-solarized-light.sh
@@ -0,0 +1,23 @@
+
+# solarized light
+echo -ne '\e]4;0;#073642\a'   # black
+echo -ne '\e]4;1;#dc322f\a'   # red
+echo -ne '\e]4;2;#859900\a'   # green
+echo -ne '\e]4;3;#b58900\a'   # yellow
+echo -ne '\e]4;4;#268bd2\a'   # blue
+echo -ne '\e]4;5;#d33682\a'   # magenta
+echo -ne '\e]4;6;#2aa198\a'   # cyan
+echo -ne '\e]4;7;#eee8d5\a'   # white (light grey really)
+echo -ne '\e]4;8;#002b36\a'   # bold black (i.e. dark grey)
+echo -ne '\e]4;9;#cb4b16\a'   # bold red
+echo -ne '\e]4;10;#586e75\a'  # bold green
+echo -ne '\e]4;11;#657b83\a'  # bold yellow
+echo -ne '\e]4;12;#839496\a'  # bold blue
+echo -ne '\e]4;13;#6c71c4\a'  # bold magenta
+echo -ne '\e]4;14;#93a1a1\a'  # bold cyan
+echo -ne '\e]4;15;#fdf6e3\a'  # bold white
+
+echo -ne '\e]10;#586e75\a'  # foreground
+echo -ne '\e]11;#fdf6e3\a'  # background
+echo -ne '\e]12;#eee8d5\a'  # cursor
+
